7683b09ab28e1f3e8dbb6fd72f21c459cff2d6ae
Jesse Ditson <jesse.ditson@gmail.com>
2012-05-14 17:13:12 -0700

    making config clone config to prevent references modifying file

--- proxy.js
+++ proxy.js
@@ -1,10 +1,23 @@
 // Dependencies
 // ------------
 
+var cloneConfig = function(o){
+  var r = {};
+  for(var k in o){
+    if(!o.hasOwnProperty(k)) continue;
+    var t = typeof o[k];
+    if(t === 'object'){
+      r[k] = cloneConfig(o[k]);
+    } else if(t !== 'function'){
+      r[k] = o[k];
+    }
+  }
+  return r;
+}
+
 var proxy = require('http-proxy'),
-  config = require('config'),
-  fs = require('fs');
-  
+    config = cloneConfig(require('config')),
+    fs = require('fs');
 // Set up logger.
 var logger = require('./lib/logger.js');
 
